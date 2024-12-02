8a4a8286a4d4fa5841f8b79c8fa28f62fad4e54e
Lauren <laurenebrown98@gmail.com>
2024-12-01 20:50:11 -0500

    func

---
+++ 2024/dec1/part_1.go
@@ -0,0 +1,23 @@
+package dec1
+
+import (
+	"math"
+	"sort"
+)
+
+
+
+func distance_sum(team_a []int, team_b []int) int {
+	sort.Ints(team_a)
+	sort.Ints(team_b)
+
+	sum := 0.0
+
+	for i := range team_a {
+		add := math.Abs(float64(team_a[i] - team_b[i]))
+		sum = sum + add
+	}
+
+	return int(sum)
+}
+
---
+++ 2024/go.mod
@@ -0,0 +1,4 @@
+module github.com/lbrown98/advent-of-code
+
+go 1.22.6
+
