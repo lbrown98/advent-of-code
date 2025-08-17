f302cdd326d4705678e194d383e0c2c7605a6629
Lauren <laurenebrown98@gmail.com>
2025-08-17 19:46:32 -0400

    day3

--- main.go
+++ main.go
@@ -4,14 +4,23 @@ import (
 	"fmt"
 	"github.com/lbrown98/advent-of-code/2024/dec1"
 	"github.com/lbrown98/advent-of-code/2024/dec2"
+	"github.com/lbrown98/advent-of-code/2024/dec3"
 	"github.com/lbrown98/advent-of-code/2024/config"
 )
 
 func main() {
-	day2()
+	day3()
 }
 
-func day2() {
+func day3() {
+	total := dec3.ParseCorruption(config.CorruptMemory)
+	fmt.Println("total: ",total)
+
+	total = dec3.ParseCorruptedMemoryWithInstructions(config.CorruptMemory)
+	fmt.Println("total: ",total)
+}
+
+func day2(){
 	safeReports := dec2.SafeReports(config.Levels, false)
 	fmt.Println("safe reports: ", safeReports)
 
@@ -19,7 +28,7 @@ func day2() {
 	fmt.Println("dampened safe reports: ", dampenedSafeReports)
 }
 
-func day1() {
+func day1(){
 	distance := dec1.DistanceSum(config.Team_a, config.Team_b)
 	fmt.Println("distance: ", distance)
 
